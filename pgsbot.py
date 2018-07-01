4f3f49f4216ea7cd01d16257a360860cfb8d4df4
PeakeyP <PeakeyP@users.noreply.github.com>
2018-07-01 07:00:18 +0100

    Connect to Discord and print ready message
    Creates a new Class for our bot which extends the Discord Client.
    
    Listens to the on_ready event to print a message once the Discord login
    process has been completed.
    
    Don't forget to replace the API_KEY with your real Discord bot token.

--- pgsbot.py
+++ pgsbot.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
 
+import discord
+
+class PgsBot(discord.Client):
+    async def on_ready(self):
+        print('Logged in as {0}.'.format(self.user))
+
+client = PgsBot()
+client.run('API_KEY')
