c9f4c0f3856947f4142978b43b9352bf123748d8
PeakeyP <PeakeyP@users.noreply.github.com>
2018-07-01 14:32:40 +0100

    Add loading/connected status to console output
    As soon as the PgsBot class is initialised, we output a loading message
    to the console window so we know it's doing something.
    
    We then create a background task which will run immediately after. Once
    the bg_task method sees that Discord is ready to rock, it now tells us.

--- pgsbot.py
+++ pgsbot.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 
+import asyncio
 import discord
 
 class PgsBot(discord.Client):
+    def __init__(self, *args, **kwargs):
+        print('Loading...')
+
+        super().__init__(*args, **kwargs)
+        self.bg_task = self.loop.create_task(self.bg_task())
+
     async def on_ready(self):
         print('Logged in as {0}.'.format(self.user))
 
@@ -18,6 +25,11 @@ class PgsBot(discord.Client):
 
         if message.content == '!ping':
             await message.channel.send('pong')
+
+    async def bg_task(self):
+        await self.wait_until_ready()
+
+        print("Connected!")
 
 client = PgsBot()
 client.run('API_KEY')
